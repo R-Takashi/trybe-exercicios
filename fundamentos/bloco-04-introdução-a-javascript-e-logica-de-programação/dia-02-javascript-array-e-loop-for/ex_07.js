2c2c479090e38c2095b51b45c2c70f8aa56d40b9
Rodrigo Takashi Condo <rodrigo0takashi@gmail.com>
2022-02-04 15:15:07 -0300

    Javascript  - exercicio 07

---
+++ fundamentos/bloco-04-introdução-a-javascript-e-logica-de-programação/dia-02-javascript-array-e-loop-for/ex_07.js
@@ -0,0 +1,10 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let menorValor = numbers[0];
+
+for ( let index = 1 ; index < numbers.length ; index += 1){
+    if (menorValor > numbers[index]){
+        menorValor = numbers[index];
+    }
+}
+
+console.log(menorValor);
