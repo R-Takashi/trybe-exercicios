6a2078d000d5d0795ef953cc882807aceb35fb5d
Rodrigo Takashi Condo <rodrigo0takashi@gmail.com>
2022-02-08 10:33:56 -0300

    Javascript dia 04 - Exercicio 07

---
+++ fundamentos/bloco-04-introdução-a-javascript-e-logica-de-programação/dia-04-javascript-objetos-e-funçoes/ex-07.js
@@ -0,0 +1,24 @@
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+  let tituloLivro = leitor['livrosFavoritos'][0]['titulo'];
+
+  console.log('O livro favorito de ' + leitor['nome'] + ' ' + leitor['sobrenome'] + ' se chama ' + tituloLivro);
+
+  leitor.livrosFavoritos.push(
+    {
+        titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+        autor: 'JK Rowling',
+        editora: 'Rocco',
+      }
+  );
