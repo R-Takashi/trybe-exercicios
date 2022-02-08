ff0a1cd2b27c5aa5d6ce356f48e6a111827df382
Rodrigo Takashi Condo <rodrigo0takashi@gmail.com>
2022-02-07 23:24:05 -0300

    Javascript dia 03 - Exercicio 01 / 02 Bonus

---
+++ fundamentos/bloco-04-introdução-a-javascript-e-logica-de-programação/dia-03-javascript-logica-de-programação-e-algoritmos/ex-01B.js
@@ -0,0 +1,10 @@
+let n = 5;
+let figura = "" ;
+
+for(let index = 0 ; index < n ; index += 1){
+    figura = figura + "*";
+}
+
+for(let index = 0 ; index < n ; index += 1){
+    console.log(figura);
+}
